95a692a43fea38d0e14d4b9e62b2af04f3d8cf05
venfran <fgvr92@gmail.com>
2024-04-13 16:37:26 -0700

    fix: styles for desktop

---
+++ src/utils/data/benefits.ts
@@ -0,0 +1,16 @@
+export const benefits = [
+  {
+    image: '/images/plumber-1.png',
+    description:
+      '24/7 availability ensuring your appliance issues are promptly addressed.',
+  },
+  {
+    image: '/images/plumber-2.png',
+    description:
+      "From diverse brands to various problems, we've got you covered.",
+  },
+  {
+    image: '/images/plumber-3.png',
+    description: 'Browse through our Google reviews to see satisfied customers',
+  },
+];
