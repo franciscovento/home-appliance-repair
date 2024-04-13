b6c5ec4dd28071decce2f9399d475234af09ed12
venfran <fgvr92@gmail.com>
2024-04-13 16:05:09 -0700

    feat: adding last 3 sections

---
+++ src/utils/data/Comments.ts
@@ -0,0 +1,42 @@
+export const comments = [
+  {
+    id: 1,
+    content:
+      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
+    url: 'https://www.google.com',
+    name: 'John Doe',
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 2,
+    content:
+      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
+    url: 'https://www.google.com',
+    name: 'John Doe',
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 3,
+    content:
+      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
+    url: 'https://www.google.com',
+    name: 'John Doe',
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 4,
+    content:
+      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
+    url: 'https://www.google.com',
+    name: 'John Doe',
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 5,
+    content:
+      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
+    url: 'https://www.google.com',
+    name: 'John Doe',
+    image: 'https://via.placeholder.com/150',
+  },
+];
