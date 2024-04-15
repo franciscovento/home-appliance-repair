65edf589a607d998089fa5916c39adb06f0eaf39
venfran <fgvr92@gmail.com>
2024-04-15 12:57:30 -0700

    feat: adding google reviews

--- src/utils/data/Comments.ts
+++ src/utils/data/Comments.ts
@@ -2,41 +2,57 @@ export const comments = [
   {
     id: 1,
     content:
-      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
-    url: 'https://www.google.com',
-    name: 'John Doe',
-    image: 'https://via.placeholder.com/150',
+      "We had the pleasure of working with SmartNest Appliance Repair to install a reverse osmosis system in my home, and we couldn't be happier with the results. From start to finish we witnessed professionalism, expertise, and excellent customer service...",
+    url: 'https://g.co/kgs/wLQvGiB',
+    name: 'Swathi Twaddell',
+    image: '/images/comments/swathi.png',
   },
   {
     id: 2,
     content:
-      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
-    url: 'https://www.google.com',
-    name: 'John Doe',
-    image: 'https://via.placeholder.com/150',
+      'This guys are really professional and reliable! Previously other company tech came charge me for service call fee and unable to fix it saying it’s too old and part is not available. This guys are responsible and pro, my double oven was fixed in a few hours. Will definitely use them again. Thank you so much',
+    url: 'https://g.co/kgs/Z62TbkC',
+    name: 'Firuz N',
+    image: '/images/comments/firuz.png',
   },
   {
     id: 3,
     content:
-      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
-    url: 'https://www.google.com',
-    name: 'John Doe',
-    image: 'https://via.placeholder.com/150',
+      'Great experience working with Frank on multiple projects like replacing Food waste disposal system, replacing Air switch for the disposal. Experienced person and knows his craft well. Thank you Frank!',
+    url: 'https://g.co/kgs/XtxLFW5',
+    name: 'Amey Mahajan',
+    image: '/images/comments/amey.png',
   },
   {
     id: 4,
     content:
-      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
-    url: 'https://www.google.com',
-    name: 'John Doe',
-    image: 'https://via.placeholder.com/150',
+      'Few days ago my 10 years old dryer stopped working, I thought of buying a new set but a friend recommended that I first try Smartnest services So I thought of giving it a try, I called them and next day they came checked the dryer, he cleaned it horoughly...',
+    url: 'https://g.co/kgs/hTY7LMP',
+    name: 'Roy',
+    image: '/images/comments/roy.png',
   },
   {
     id: 5,
     content:
-      "Bay Master Appliance was a lifesaver when it came to diagnosing my dryer issues. He was on time, professional, and more courteous than anyone else in town! If you have an appliance repair issue, call them first because they are the best value around. I couldn'",
-    url: 'https://www.google.com',
-    name: 'John Doe',
-    image: 'https://via.placeholder.com/150',
+      'Frank and team were awesome! Very responsive and kind. Quick and affordable I would recommend Frank to anyone. I plan to use Frank in the future and keep him in my contacts. Thank you Frank for your clear, expertise and taking care of my garbage disposal.',
+    url: 'https://g.co/kgs/vbe2ak2',
+    name: 'Tammy Sanders',
+    image: '/images/comments/tammy.png',
+  },
+  {
+    id: 6,
+    content:
+      'I contacted them for repairing our dishwasher. Frank was quick to respond and arrived to look at the issue the very next day. He gave a decent quote and kept me informed when the parts are arriving. All in all it was great working with him.',
+    url: 'https://g.co/kgs/pDChmyV',
+    name: 'Vivek Bhardwaj',
+    image: '/images/comments/vivek.png',
+  },
+  {
+    id: 7,
+    content:
+      'The best service ever! I been struggling trying to find a good company with fair prices, I def recommend Home Appliance Repair 👩‍🔧',
+    url: 'https://g.co/kgs/LhNiJ4t',
+    name: 'Lua Del Carp',
+    image: '/images/comments/lua.png',
   },
 ];
