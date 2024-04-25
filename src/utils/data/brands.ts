ee1266927ca73b26af977efffd4f9ce5d9dc0fc6
venfran <fgvr92@gmail.com>
2024-04-24 17:16:45 -0700

    feat: adding brands to website

---
+++ src/utils/data/brands.ts
@@ -0,0 +1,8 @@
+export const brands = [
+  { id: 1, name: 'Bosch', image: '/images/brands/bosch.png' },
+  { id: 2, name: 'Jen Air', image: '/images/brands/jennair.png' },
+  { id: 3, name: 'Kitchen Aid', image: '/images/brands/kitchenaid.png' },
+  { id: 4, name: 'Samsung', image: '/images/brands/samsung.png' },
+  { id: 5, name: 'Thermador', image: '/images/brands/thermador.png' },
+  { id: 6, name: 'Kenmore', image: '/images/brands/kenmore.png' },
+];
