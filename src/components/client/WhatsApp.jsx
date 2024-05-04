a92807a0578237b59c90ce2f49fa920d45eb741c
venfran <fgvr92@gmail.com>
2024-05-03 17:51:57 -0700

    feat: changue whatsapp number

--- src/components/client/WhatsApp.jsx
+++ src/components/client/WhatsApp.jsx
@@ -1,11 +1,12 @@
 import { FloatingWhatsApp } from 'react-floating-whatsapp';
-import whatsAppImage from '../../assets/images/icon.png';
+import whatsAppImage from '../../assets/images/thumbnails/icon.png';
 
 const WhatsApp = () => {
   return (
     <FloatingWhatsApp
-      phoneNumber="+5491126238672"
+      phoneNumber="+14089091844"
       accountName="Home Appliance Repair"
+      statusMessage="Typically replies within 5 minutes"
       allowEsc={true}
       avatar={whatsAppImage.src}
     />
